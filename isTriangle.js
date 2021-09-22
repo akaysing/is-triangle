bf34cc92a83839d4aebace510beaab4fe41e2007
Adarsh Kumar Singh <adarshk03.singh@gmail.com>
2021-09-23 00:00:18 +0530

    beautify file

--- isTriangle.js
+++ isTriangle.js
@@ -5,24 +5,23 @@ const message = document.querySelector(".display-message");
 
 const buttonCheck = document.querySelector(".btn-check");
 
-buttonCheck.addEventListener("click", function checkTriangle(){
+buttonCheck.addEventListener("click", function checkTriangle() {
     hideMessage();
-    const sumOfAngles = firstAngle.value + secondAngle.value + thirdAngle.value ;
-    if(sumOfAngles === 180){
-          showMessage("Yayy, the angles form a triangle");
-    }
-    else {
+    const sumOfAngles = firstAngle.value + secondAngle.value + thirdAngle.value;
+    if (sumOfAngles === 180) {
+        showMessage("Yayy, the angles form a triangle");
+    } else {
         showMessage("Oh! no, the angles do not form a triangle");
     }
 
-    function hideMessage(){
+    function hideMessage() {
         message.style.display = "none";
     }
 
-    function showMessage(msg){
+    function showMessage(msg) {
         message.style.display = "block";
         message.innerText = msg;
     }
 
-    
+
 })
