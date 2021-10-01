922c54a078aca6affec38bda594c9a9b2f834e94
Adarsh Kumar Singh <adarshk03.singh@gmail.com>
2021-10-01 21:16:46 +0530

    completed quiz

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizform = document.querySelector(".main-container");
+const submitBtn = document.querySelector(".btn-submit");
+const outputMessage = document.querySelector(".output");
+
+const correctAnswers = ["90°", "right"];
+
+function calculateScore() {
+  const formResults = new FormData(quizform);
+  let score = 0,
+    index = 0;
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  // console.log("The score is "+score);
+  outputMessage.innerText = `The Score is ${score}`;
+}
+
+submitBtn.addEventListener("click", calculateScore);
